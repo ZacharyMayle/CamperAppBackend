8517d623b3baee255a63b65e041d86b46db57880
github username <github email address>
2020-02-10 15:32:51 -0700

    Added crud for User

--- camper_backend/app/controllers/users_controller.rb
+++ camper_backend/app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+    
+    def index
+        @users = User.all
+        render json: @users
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        redner json: @user
+    end
+    
+    def create
+        @user = User.create(
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            current_state: params[:current_state]
+        )
+        render json: @user
+    end
+    
+    def update
+        @user = User.find(params[:id])
+
+        @user.update(
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            current_state: params[:current_state]
+        )
+
+        render json: @user
+    end
+    
+    def delete
+        @user = find(params[:id])
+
+        @user.destroy
+
+        render body: nil, status: :no_content 
+    end
 end
