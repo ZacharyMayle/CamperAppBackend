c174e3d7a9f0ea579a47b0f841cdae3eb747e1f3
github username <github email address>
2020-02-10 14:49:54 -0700

    Going back to when rails was working

--- camper_backend/db/seeds.rb
+++ camper_backend/db/seeds.rb
@@ -2,7 +2,6 @@ require 'pry'
 require 'rest-client'
 
 
-!!
 # parks = RestClient.get "https://developer.nps.gov/api/v1/parks?stateCode=CO&api_key=3h90U6sxs7uzFVrhFU5PBItP0XZKV2cJWikfOeE5"
 # parsed_json = JSON.parse(parks)
 # parsed_parks = parsed_json["data"].map do |parsed_json|
@@ -14,5 +13,7 @@ parsed_json = JSON.parse(campgrounds)
 parsed_campgrounds = parsed_json["data"].map do |campground|
     campground
 end
+
+
 
 binding.pry
