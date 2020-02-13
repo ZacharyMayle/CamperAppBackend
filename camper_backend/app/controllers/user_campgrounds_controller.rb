3dd460ea39a49df536ef13d04efbb5da1c026d97
github username <github email address>
2020-02-13 00:25:13 -0700

    fixed issues with create method on user_campground controller and added direct_back

--- camper_backend/app/controllers/user_campgrounds_controller.rb
+++ camper_backend/app/controllers/user_campgrounds_controller.rb
@@ -11,13 +11,14 @@ class UserCampgroundsController < ApplicationController
 
     def create
         @user_campground = UserCampground.create(
-            user: params[:user],
-            campground: params[:campground],
+            user_id: params[:user_id],
+            campground_id: params[:campground_id],
             camping_duration: params[:camping_duration]
         )
-        render json: @user_campground
+        # render json: @user_campground
+        redirect_back(fallback_location: "http://localhost:3001")
     end
-
+ 
     def update
         @user_campground = UserCampground.find(params[:id])
         @user_campground.update(
